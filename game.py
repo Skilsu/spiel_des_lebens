f151bca77abad0f002974ab17718ae094ce8c63f
Abdülkerim Kilinc <abdulkerimkilinc@outlook.de>
2023-07-31 23:53:00 +0200

    implementing Glücksrad (only image, it has no function)

--- game.py
+++ game.py
@@ -1,10 +1,14 @@
 import pygame
 import sys
+import math
 
 # Spiel-Parameter
-SCREEN_SIZE = (800, 600)
+SCREEN_SIZE = (1400, 800)
 BACKGROUND_COLOR = (0, 0, 0)
 PLAYER_COLOR = (0, 0, 255)
+WHEEL_RADIUS = 100 #Größe des Rades
+WHEEL_POSITION = (1030,380)
+WHEEL_ROTATION_SPEED = 5
 
 
 class Game:
@@ -15,16 +19,72 @@ class Game:
         self.clock = pygame.time.Clock()
         self.player = Player(SCREEN_SIZE[0] // 2, SCREEN_SIZE[1] // 2)
         self.board_image = pygame.image.load('spiel des lebens spielbrett.jpg')
-        self.board_image = pygame.transform.scale(self.board_image, SCREEN_SIZE)
+        self.board_image = pygame.transform.scale(self.board_image, (1100,800))
+
+        self.font = pygame.font.Font(None, 30)
+        self.wheel_angle = 0
+        self.selected_number = 0
+
+    def get_color(self, number):
+        # Definieren Sie Ihre Farben hier
+        colors = [
+            (168, 0, 185),  # Lila
+            (255, 0, 255),  # pink
+            (255, 0, 0),  # rot
+            (255, 73, 0),  # rot-orange
+            (255, 109, 0),  # orange-rot
+            (255, 146, 0),  # orange
+            (255, 182, 0),  # orange-gelb
+            (255, 219, 0),  # gelb-orange
+            (255, 255, 0),  # gelb
+            (200, 255, 0)  # gelb-grün
+
+            # usw., fügen Sie mehr Farben hinzu, wenn Sie mehr als 6 Abschnitte haben
+        ]
+        return colors[number % len(colors)]
+
+    def draw_wheel(self):
+        for i in range(10):
+            angle_start = math.radians(self.wheel_angle + i * 360 / 10)
+            angle_end = math.radians(self.wheel_angle + (i + 1) * 360 / 10)
+            color = self.get_color(i)
+            pygame.draw.arc(self.screen, color,
+                            pygame.Rect(WHEEL_POSITION[0] - WHEEL_RADIUS, WHEEL_POSITION[1] - WHEEL_RADIUS,
+                                        2 * WHEEL_RADIUS, 2 * WHEEL_RADIUS), angle_start, angle_end, WHEEL_RADIUS)
+
+        for i in range(10):
+            angle = math.radians(self.wheel_angle + i * 360 / 10)
+            start_pos = (
+                WHEEL_POSITION[0] + WHEEL_RADIUS * math.cos(angle),
+                WHEEL_POSITION[1] + WHEEL_RADIUS * math.sin(angle)
+            )
+            end_pos = (
+                WHEEL_POSITION[0] + WHEEL_RADIUS * 0.9 * math.cos(angle),
+                WHEEL_POSITION[1] + WHEEL_RADIUS * 0.9 * math.sin(angle)
+            )
+            pygame.draw.line(self.screen, (255, 255, 255), start_pos, end_pos, 2)
+            number_surface = self.font.render(str(i + 1), True, (0, 0, 0))
+            number_pos = (
+                WHEEL_POSITION[0] + WHEEL_RADIUS * 0.8 * math.cos(angle),
+                WHEEL_POSITION[1] + WHEEL_RADIUS * 0.8 * math.sin(angle)
+            )
+            self.screen.blit(number_surface, number_pos)
+        #selected_number_surface = self.font.render(str(self.selected_number), True, (0, 0, 0))
+        #self.screen.blit(selected_number_surface, WHEEL_POSITION)
 
     def run(self):
-        while True:
+        running = True
+        while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    pygame.quit()
-                    sys.exit()
+                    running = False
+                    #pygame.quit()
+                    #sys.exit()
 
-            self.screen.blit(self.board_image, (0,0))
+
+
+            self.screen.blit(self.board_image, (300,0))
+            self.draw_wheel()
             #self.player.draw(self.screen)
 
             pygame.display.flip()
